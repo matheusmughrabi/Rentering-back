981d389b22f893cf9f48556070bf6774c82c10aa
Matheus <matheus.campanini@predify.com>
2021-04-12 13:47:58 -0300

    - Creates RenterController - Creates CreateRenter endpoint

--- Rentering.Contracts.Application/Commands/RenterCommands.cs
+++ Rentering.Contracts.Application/Commands/RenterCommands.cs
@@ -1,12 +1,12 @@
 ﻿using Rentering.Common.Shared.Commands;
 using Rentering.Contracts.Domain.Enums;
+using System.Text.Json.Serialization;
 
 namespace Rentering.Contracts.Application.Commands
 {
     public class CreateRenterCommand : ICommand
     {
         public CreateRenterCommand(
-            int accountId, 
             string firstName, 
             string lastName, 
             string nationality, 
@@ -25,7 +25,6 @@ namespace Rentering.Contracts.Application.Commands
             string spouseIdentityRG,
             string spouseCPF)
         {
-            AccountId = accountId;
             FirstName = firstName;
             LastName = lastName;
             Nationality = nationality;
@@ -45,23 +44,24 @@ namespace Rentering.Contracts.Application.Commands
             SpouseCPF = spouseCPF;
         }
 
-        public int AccountId { get; private set; }
-        public string FirstName { get; private set; }
-        public string LastName { get; private set; }
-        public string Nationality { get; private set; }
-        public string Ocupation { get; private set; }
-        public e_MaritalStatus MaritalStatus { get; private set; }
-        public string IdentityRG { get; private set; }
-        public string CPF { get; private set; }
-        public string Street { get; private set; }
-        public string Bairro { get; private set; }
-        public string Cidade { get; private set; }
-        public string CEP { get; private set; }
-        public string Estado { get; private set; }
-        public string SpouseFirstName { get; private set; }
-        public string SpouseLastName { get; private set; }
-        public string SpouseNationality { get; private set; }
-        public string SpouseIdentityRG { get; private set; }
-        public string SpouseCPF { get; private set; }
+        [JsonIgnore]
+        public int AccountId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Nationality { get; set; }
+        public string Ocupation { get; set; }
+        public e_MaritalStatus MaritalStatus { get; set; }
+        public string IdentityRG { get; set; }
+        public string CPF { get; set; }
+        public string Street { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string CEP { get; set; }
+        public string Estado { get; set; }
+        public string SpouseFirstName { get; set; }
+        public string SpouseLastName { get; set; }
+        public string SpouseNationality { get; set; }
+        public string SpouseIdentityRG { get; set; }
+        public string SpouseCPF { get; set; }
     }
 }
---
+++ Rentering.Contracts.Infra/Repositories/CUDRepositories/RenterCUDRepository.cs
@@ -0,0 +1,31 @@
+﻿using Rentering.Common.Infra;
+using Rentering.Contracts.Domain.Entities;
+using Rentering.Contracts.Domain.Repositories.CUDRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rentering.Contracts.Infra.Repositories.CUDRepositories
+{
+    public class RenterCUDRepository : IRenterCUDRepository
+    {
+        private readonly RenteringDataContext _context;
+
+        public RenterCUDRepository(RenteringDataContext context)
+        {
+            _context = context;
+        }
+
+        public bool CheckIfAccountExists(int accountId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateRenter(RenterEntity renter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
